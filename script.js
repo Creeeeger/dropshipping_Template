81a59b69112d5c2838da1031d57255f4ee122e94
Gregor <gregor@MacBook-Pro-von-Greg.local>
2023-11-05 16:13:37 +0000

    reformat

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function ImageSwap(imageSrc, dataCard) {
-  document.querySelector('.' + dataCard).src = imageSrc;
+    document.querySelector('.' + dataCard).src = imageSrc;
 }
 
 const menuLinks = document.querySelectorAll('.content');
 menuLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetSectionId = event.currentTarget.getAttribute('href');
-    const targetSection = document.querySelector(targetSectionId);
-    targetSection.scrollIntoView({
-      behavior: 'smooth'
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetSectionId = event.currentTarget.getAttribute('href');
+        const targetSection = document.querySelector(targetSectionId);
+        targetSection.scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-  });
 });
